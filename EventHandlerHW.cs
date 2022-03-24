5c2d9716a2c12599b2d4194e281ca3666b91727d
eleborrr <56829513+eleborrr@users.noreply.github.com>
2022-03-24 14:22:58 +0300

    EventHandler HW

---
+++ EventHandlerHW.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EventHandlerHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subscriber = new Subscriber();
+            subscriber.SomeMethod();
+        }
+    }
+
+    class Subscriber
+    {
+        public void SomeMethod()
+        {
+            var publisher = new Publisher();
+            EventHandler<char> handler = (o, a) => Console.WriteLine("Ввёденo: {0}", a);
+            publisher.OnKeyPressed += handler;
+            publisher.Run();
+        }
+    }
+
+    class Publisher
+    {
+        public EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+            while(true)
+            {
+                var symbol = Convert.ToChar(Console.ReadLine().ToLower());
+                if (symbol == 'c')
+                    break;
+                else
+                    OnKeyPressed?.Invoke(this, symbol);
+            }
+        }
+    }
+}
