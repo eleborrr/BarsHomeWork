b8009f5dbd42af19eae6d4142bb95ace1349a558
eleborrr <56829513+eleborrr@users.noreply.github.com>
2022-03-31 23:46:33 +0300

    HomeWork2

---
+++ Entity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarsHomeWork2
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Name { get; set; }
+
+        public static Dictionary<int, List<Entity>> ConvertToDictionary(List<Entity> collection)
+        {
+            var result = new Dictionary<int, List<Entity>>();
+            foreach(var elem in collection.Select(x => x.ParentId))
+                result[elem] = collection.Where(x => x.ParentId == elem).ToList();
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"[Entity] Id: '{Id}', ParentId: '{ParentId}', Name: '{Name}'";
+        }
+    }
+}
