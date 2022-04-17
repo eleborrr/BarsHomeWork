1d0117b8f98f17222a637c84de57596ecc49ca1d
eleborrr <56829513+eleborrr@users.noreply.github.com>
2022-04-17 17:05:03 +0300

    multithreading hw

---
+++ MultithreadingTask/Program.cs
@@ -0,0 +1,57 @@
+﻿using static MultiThreadingTask.Requester;
+using System.Threading;
+
+namespace MultiThreadingTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Приложение запущено");
+
+
+            var handler = new Handler();
+            
+            var command = "";
+
+            while (command != "/exit")
+            {
+                Console.WriteLine("Введите текст запроса для отправки. Для выхода введите /exit");
+                command = Console.ReadLine();
+                var id = Guid.NewGuid().ToString("D");
+                Console.WriteLine("Введите аргументы сообщения. Если аргументы не нужны - введите /end");
+                var arg = Console.ReadLine();
+                var _args = new List<string>();
+                while (arg != "/end")
+                {
+                    _args.Add(arg);
+                    Console.WriteLine("Введите следующий аргумент сообщения. Для окончания добавления аргументов введите /end");
+                    arg = Console.ReadLine();
+                }
+                Console.WriteLine($"Было послано сообщение \'{command}\'. Присвоен идентификатор {id}");
+                ThreadPool.QueueUserWorkItem(callback => Console.WriteLine($"Сообщение с идентификатором {id} {handler.HandleRequest(command, _args.ToArray())}"));
+            }
+        }
+        
+    }
+
+    public class Handler : IRequestHandler
+    {
+        public string HandleRequest(string message, string[] arguments)
+        {
+            try
+            {
+                Thread.Sleep(1000);
+                if (arguments.Contains("упади"))
+                {
+                    throw new Exception("Я упал, как сам просил");
+                }
+                return $"— получило сообщение { Guid.NewGuid().ToString("D")}";
+            }
+            catch(Exception ex)
+            {
+                return $"упало с сообщением {ex.Message}";
+            }
+        }
+    }
+}
---
+++ MultithreadingTask/Requester.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadingTask
+{
+    internal class Requester
+    {
+        /// <summary>
+        /// Предоставляет возможность обработать запрос.
+        /// </summary>
+        public interface IRequestHandler
+        {
+            /// <summary>
+            /// Обработать запрос.
+            /// </summary>
+            /// <param name="message">Сообщение.</param>
+            /// <param name="arguments">Аргументы запроса.</param>
+            /// <returns>Результат выполнения запроса.</returns>
+            public string HandleRequest(string message, string[] arguments);
+        }
+
+
+        /// <summary>
+        /// Тестовый обработчик запросов.
+        /// </summary>
+        public class DummyRequestHandler : IRequestHandler
+        {
+            /// <inheritdoc />
+            public string HandleRequest(string message, string[] arguments)
+            {
+                // Притворяемся, что делаем что то.
+                Thread.Sleep(10_000);
+                if (message.Contains("упади"))
+                {
+                    throw new Exception("Я упал, как сам просил");
+                }
+                return Guid.NewGuid().ToString("D");
+            }
+        }
+    }
+}
